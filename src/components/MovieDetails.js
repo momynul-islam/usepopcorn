d64bf52f8361791c365460c44b66dba920aefcd4
momynul-islam <islammomynul.cod@gmail.com>
2024-01-08 08:42:31 +0600

    final

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -81,7 +81,7 @@ export default function MovieDetails({
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}&i=${selectedId}`
         );
         const data = await res.json();
 
