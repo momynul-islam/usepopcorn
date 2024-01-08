6776a8c8f5e4ddb9ba5dd8cd24b7b706716aea5c
momynul-islam <islammomynul.cod@gmail.com>
2024-01-08 08:38:15 +0600

    nothing

--- src/components/App.js
+++ src/components/App.js
@@ -64,7 +64,7 @@ export default function App() {
           setError("");
 
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${query}`,
             { signal: controller.signal }
           );
 
